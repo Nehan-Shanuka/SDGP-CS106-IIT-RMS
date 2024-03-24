e94edf26805dbc7cf51523646e5f94002c0cbc38
Thaveesha Thathsara <thaveesha20222110@iit.ac.lk>
2024-03-25 00:37:09 +0530

    Change userProfile Clours,buttons and posetions

--- frontend/src/pages/UserProfile/index.jsx
+++ frontend/src/pages/UserProfile/index.jsx
@@ -30,15 +30,15 @@ function ProfilePage({ userFromDB, onLogout }) {
   return (
     <div>
       <div className="flex justify-end mt-[-30px]">
-        <div className="flex justify-end pr-5 pt-5 space-x-2">
+        <div className="flex justify-end pt-5 pr-5 space-x-2">
           {/* <Button variant="contained" color="info" style={{ marginRight: '8px' }}>
       Update Profile
       </Button> */}
         </div>
       </div>
-      <section className="container py-2 mt-10">
+      <section className="container py-2 mt-5">
         <div
-          className="flex flex-col lg:flex-row justify-center items-start gap-10"
+          className="flex flex-col items-start justify-center gap-10 lg:flex-row"
           style={{
             display: "flex",
             flexDirection: "column",
@@ -46,25 +46,26 @@ function ProfilePage({ userFromDB, onLogout }) {
             marginTop: "auto",
           }}
         >
-          <div className="w-full lg:w-1/4 bg-gray-300 rounded-lg p-8 text-center">
+          <div className="w-full p-8 text-center bg-gray-600 rounded-lg lg:w-1/4">
             <img
               src={userData.profilePicture}
               alt="avatar"
-              className="rounded-full w-36 h-34 mx-auto mb-4"
+              className="mx-auto mb-4 rounded-full w-36 h-34"
             />
-            {<p className="text-gray-600 mb-1">{userFromDB.name}</p>}
+            {<p className="mb-1 text-white">{userFromDB.name}</p>}
             <div className="flex justify-center gap-1">
-              {/* <button className="bg-blue-500 text-white py-2 px-5 rounded">{userData.level}</button>
-              <button className="bg-blue-500 text-white py-2 px-5 rounded">{userData.course}</button> */}
+              {/* <button className="px-5 py-2 text-white bg-blue-500 rounded">{userData.level}</button>
+              <button className="px-5 py-2 text-white bg-blue-500 rounded">{userData.course}</button> */}
             </div>
           </div>
 
           <div
-            className="w-full  lg:w-2/3 bg-gray-300 rounded-lg p-7"
+            className="w-full bg-gray-600 rounded-lg lg:w-2/3 p-7"
             style={{
               flexDirection: "column",
               alignItems: "center",
               marginTop: "auto",
+              color: "white",
             }}
           >
             <UserInfo label="Full Name : " value={userFromDB.name} />
@@ -87,7 +88,7 @@ function UserInfo({ label, value }) {
   return (
     <div className="mb-4">
       <div className="flex">
-        <span className="w-1/3 text-gray-600">{label}</span>
+        <span className="w-1/3 text-gray-100">{label}</span>
         <span className="flex-1 text-muted">{value}</span>
       </div>
       <hr className="my-4" />
