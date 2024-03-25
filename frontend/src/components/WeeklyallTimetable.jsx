57f4586482ac80b13323dda877196f75c63d8c18
Sanuth Dasanyaka <sanuthsasmin7@gmail.com>
2024-03-25 08:31:23 +0530

    ui ux redisgn

--- frontend/src/components/WeeklyallTimetable.jsx
+++ frontend/src/components/WeeklyallTimetable.jsx
@@ -1,15 +1,13 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-// import RedirectButton from "../RedirectButto";
 import axios from "axios";
 import Tooltip from "@mui/material/Tooltip";
+import Lottie from "lottie-react";
+import Loading from "../assets/loading.json";
 
-// Styled component for individual timetable item
 const Item = styled("div")(({ theme, color }) => ({
   backgroundColor: color,
   ...theme.typography.body2,
@@ -21,13 +19,12 @@ const Item = styled("div")(({ theme, color }) => ({
   margin: "10px",
 }));
 
-// Component for displaying the weekly timetable
 const WeeklyallTimetable = ({ selectedValue }) => {
   const [timetableData, setTimetableData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [showLoader, setShowLoader] = useState(true); // New state for controlling loader visibility
 
-  // Fetch timetable data from the server on component mount
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -35,6 +32,10 @@ const WeeklyallTimetable = ({ selectedValue }) => {
         console.log("Response data:", response.data);
         setTimetableData(response.data);
         setLoading(false);
+        // Show the loader for 2 seconds
+        setTimeout(() => {
+          setShowLoader(false);
+        }, 2000);
       } catch (error) {
         console.error("Error fetching timetable data:", error);
         setError(error.message);
@@ -45,24 +46,23 @@ const WeeklyallTimetable = ({ selectedValue }) => {
     fetchData();
   }, []);
 
-  // Display loading message while fetching data
-  if (loading) {
-    return <p>Loading...</p>;
+  if (loading || showLoader) { // Check for showLoader state
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Lottie animationData={Loading} />
+      </div>
+    );
   }
 
   if (error) {
     return <p>Error: {error}</p>;
   }
 
-  // Color options for timetable items
   const colors = ["#FF9999", "#99FF99", "#9999FF", "#FFFF99"];
 
   return (
     <div className="flex flex-col">
-      <div className="grid justify-items-end">
-
-      </div>
-      {/* Container for displaying the weekly timetable */}
+      <div className="grid justify-items-end"></div>
       <Box
         sx={{
           flexGrow: 1,
@@ -76,13 +76,14 @@ const WeeklyallTimetable = ({ selectedValue }) => {
           scrollBehavior: "smooth",
           height: { xs: "77.5vh", md: null },
           "&::-webkit-scrollbar": {
-            display: "none"},
+            display: "none"
+          },
           "@media screen and (max-width: 768px)": {
             margin: "auto",
             marginTop: "20px",
             width: "90%",
-            padding: "20px",
-          },
+            padding: "20px"
+          }
         }}
       >
         <Grid
@@ -92,10 +93,11 @@ const WeeklyallTimetable = ({ selectedValue }) => {
           justifyContent="space-between"
           sx={{
             overflowX: "auto",
-            "&::-webkit-scrollbar": { display: "none" },
+            "&::-webkit-scrollbar": {
+              display: "none"
+            }
           }}
         >
-          {/* Iterate over each weekday */}
           {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].map(
             (weekday, index) => (
               <Grid item xs={12} sm={6} md={4} lg={2.4} key={index}>
@@ -103,7 +105,7 @@ const WeeklyallTimetable = ({ selectedValue }) => {
                   sx={{
                     backgroundColor: "#fff",
                     padding: "10px",
-                    borderRadius: "10px",
+                    borderRadius: "10px"
                   }}
                 >
                   <Typography
@@ -112,24 +114,21 @@ const WeeklyallTimetable = ({ selectedValue }) => {
                   >
                     {weekday}
                   </Typography>
-                  {/* Filter and map timetable data for the selected group and course */}
                   {timetableData
                     .filter(
-                      (item) =>
+                      item =>
                         item.sessions?.some(
-                          (session) => session.day === weekday
+                          session => session.day === weekday
                         ) &&
                         item.groupName === selectedValue &&
                         item.course === "BSc Computer Science"
                     )
                     .map((item, itemIndex) => (
                       <div key={itemIndex}>
-                        {/* Map sessions for the current day */}
                         {item.sessions
-                          .filter((session) => session.day === weekday)
+                          .filter(session => session.day === weekday)
                           .map((session, sessionIndex) => (
                             <div key={sessionIndex}>
-                              {/* Map time sessions for the current session */}
                               {session.timeSessions &&
                                 Object.keys(session.timeSessions).map(
                                   (timeKey, colorIndex) => (
@@ -139,51 +138,51 @@ const WeeklyallTimetable = ({ selectedValue }) => {
                                         session.timeSessions[timeKey]?.lecturer
                                       }
                                     >
-                                      {/* Render individual timetable item */}
                                       <Item
                                         color={
                                           session.timeSessions[timeKey] === null
                                             ? "#723E7A"
                                             : "#3E737A"
                                         }
-                                        // style={{ fontSize: "15px" }}
                                         sx={{ color: "#fff" }}
                                       >
-                                        {/* <span style={{ fontSize: "20px" }}> */}
-                                          <div className="flex justify-between mx-0">
-                                            <div className="text-[1.4rem]">
-                                              {
-                                                session.timeSessions[timeKey]
-                                                  ?.hallID
-                                              }
-                                            </div>
-                                            <div className="flex items-center bg-[#D9D9D9] w-fit text-black rounded-md text-[1.2rem] text-center px-1 align-center">
-                                              <div>
-                                                {session.timeSessions[
+                                        <div className="flex justify-between mx-0">
+                                          <div className="text-[1.4rem]">
+                                            {
+                                              session.timeSessions[timeKey]
+                                                ?.hallID
+                                            }
+                                          </div>
+                                          <div className="flex items-center bg-[#D9D9D9] w-fit text-black rounded-md text-[1.2rem] text-center px-1 align-center">
+                                            <div>
+                                              {session.timeSessions[
                                                 timeKey
                                               ]?.type
                                                 .replace("Lecture", "LEC")
                                                 .replace("Tutorial", "TUT")}
-                                              </div>
                                             </div>
-                                            <div className="flex items-center bg-[#D9D9D9] w-fit text-black rounded-full text-[1.2rem] text-center px-1 align-center">
-                                              <div>
-                                                {
+                                          </div>
+                                          <div className="flex items-center bg-[#D9D9D9] w-fit text-black rounded-full text-[1.2rem] text-center px-1 align-center">
+                                            <div>
+                                              {
                                                 session.timeSessions[timeKey]
                                                   ?.buildingID
                                               }
-                                              </div>
-                                              
                                             </div>
                                           </div>
-                                        {/* </span> */}
+                                        </div>
                                         <div>
                                           <div className="text-sm my-1">
-                                            {timeKey === "time_01" ? "08.30 - 10.30" : timeKey === "time_02" ? "10.30 - 12.30" : timeKey === "time_03" ? "13.30 - 15.30" : "15.30 - 17.30"}
+                                            {timeKey === "time_01"
+                                              ? "08.30 - 10.30"
+                                              : timeKey === "time_02"
+                                              ? "10.30 - 12.30"
+                                              : timeKey === "time_03"
+                                              ? "13.30 - 15.30"
+                                              : "15.30 - 17.30"}
                                           </div>
                                         </div>
                                         <div className="text-base my-2">
-                                         {/* Apply font size for the subject */}
                                           {session.timeSessions[timeKey]
                                             ?.subject ===
                                           "Object Oriented Programming"
